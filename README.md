6e222ccacb8d8d6848c64da70f4e2327a9f2d5c9
Rendi Kasigi <rkasigi@gmail.com>
2017-05-07 13:18:42 +0700

    add readme, add intent to send text content

---
+++ README.md
@@ -0,0 +1,9 @@
+Indonesia Android Kejar
+=======================
+
+
+Indonesia Android Kejar Batch 3 for Beginner.
+this project is practice from IAK3 Study Group for Beginner.
+
+5 participant create different component and write it into the paper. 
+and then the last participant make it real into android project.
--- app/src/main/java/net/rkasigi/iak/layouttemanku/MainActivity.java
+++ app/src/main/java/net/rkasigi/iak/layouttemanku/MainActivity.java
@@ -1,7 +1,11 @@
 package net.rkasigi.iak.layouttemanku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonBuka = (Button) findViewById(R.id.buttonOpen);
+        buttonBuka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.putExtra(Intent.EXTRA_TEXT, ((TextView) findViewById(R.id.textViewContent)).getText() );
+                i.setType("text/plain");
+                startActivity(i);
+            }
+        });
     }
 }
